856e4bcd03d9f5450481df35443b8addadfb6ed7
cavayangtao <47524863+cavayangtao@users.noreply.github.com>
2022-05-14 14:35:21 +0800

    Add files via upload

--- PathPlanning/PotentialFieldPlanning/potential_field_bezier.py
+++ PathPlanning/PotentialFieldPlanning/potential_field_bezier.py
@@ -1,7 +1,6 @@
-
+import math
 from collections import deque
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 from math import factorial          #求阶乘库
 
@@ -33,22 +32,23 @@ def calc_repulsive_potential(x, y, ox, oy, rr, o_r):
 
     minid = -1
     dmin = float("inf")
-    for i, _ in enumerate(ox):                                      # 找到距离最近的障碍物
+    for i, _ in enumerate(ox):                                  #找到距离最近的障碍物
         d = np.hypot(x - ox[i], y - oy[i]) - o_r[i]
         if dmin >= d:
             dmin = d
             minid = i
 
     # calc repulsive potential
-    dq = np.hypot(x - ox[minid], y - oy[minid]) - o_r[minid]        # 只考虑最近障碍物的作用
+    dq = np.hypot(x - ox[minid], y - oy[minid]) - o_r[minid]        #只考虑最近障碍物的作用
 
-    if dq <= rr:                                                    # 超过阈值，则不产生作用
+    if dq <= rr:                                   #超过阈值，则不产生作用
         if dq <= 0.1:
             dq = 0.01
 
         return 0.5 * ETA * (1.0 / dq - 1.0 / rr) ** 2
     else:
         return 0.0
+
 
 def get_motion_model():
 
@@ -61,7 +61,6 @@ def get_motion_model():
               [1, -1],
               [1, 1]]
     return motion
-
 
 # def oscillations_detection(previous_ids, ix, iy):
 #     previous_ids.append((ix, iy))
@@ -81,8 +80,9 @@ def get_motion_model():
 
 def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr, o_r, xmin, xmax, ymin, ymax):
 
+    
+    
     # 计算人工势场
-
     xw = int((xmax - xmin) / reso)
     yw = int((ymax - xmin) / reso)
 
@@ -100,7 +100,6 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr, o_r, xmin, xmax, 
             pmap[ix][iy] = uf
 
     # 根据人工势场规划路径点
-
     d = np.hypot(sx - gx, sy - gy)      #计算出发点到目标点之间的距离，np.hypot 用于计算直角三角形斜边
     ix = round((sx - xmin) / reso)      #算出发点对应的刻度点（步长），并赋给当前位置点
     iy = round((sy - ymin) / reso)
@@ -110,7 +109,7 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr, o_r, xmin, xmax, 
 
     rx, ry = [sx], [sy]                 #用于保存轨迹点
     motion = get_motion_model()
-    # previous_ids = deque()              #双向队列
+    previous_ids = deque()              #双向队列
 
     while d >= reso:
         minp = float("inf")
@@ -139,6 +138,7 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr, o_r, xmin, xmax, 
         rx.append(xp)
         ry.append(yp)
 
+
         # if oscillations_detection(previous_ids, ix, iy):
         #     print("Oscillation detected at ({},{})!".format(ix, iy))
         #     break
@@ -151,7 +151,6 @@ def potential_field_planning(sx, sy, gx, gy, ox, oy, reso, rr, o_r, xmin, xmax, 
     ry.append(gy)
 
     print("Goal!!")
-
     return rx, ry
 
 def draw_axis(sx,sy,gx, gy, ox, oy, xmin, xmax, ymin, ymax, o_r):
@@ -160,46 +159,47 @@ def draw_axis(sx,sy,gx, gy, ox, oy, xmin, xmax, ymin, ymax, o_r):
 
     ax.set_xticks(np.arange(xmin, xmax + 0.3, 0.3))
     ax.set_yticks(np.arange(ymin, ymax + 0.3, 0.3))
-
     for i in range(len(ox)):
         circle = plt.Circle((ox[i], oy[i]), o_r[i], color='k', fill=True)
         plt.gcf().gca().add_artist(circle)
 
     plt.plot(sx, sy, "*r")
     plt.plot(gx, gy, "pg")
-
+    plt.grid()
 
 def main():
+    
+    print("potential_field_planning start")
 
-    xmin, xmax = -1.5, 1.5                      # 设置人工势场的边界（也是坐标轴范围）
+    xmin, xmax = -1.5, 1.5                      #设置人工势场的边界（也是坐标轴范围）
     ymin, ymax = -1.5, 1.5
 
     sx, sy = -1.4, -1.4                         # 设置出发点
     gx, gy = 1.4, 1.4                           # 设置目标点
-    grid_size = 0.02                            # 单步长度，需要大于0.08
+    grid_size = 0.02                            # 单步长度
     robot_radius = 0.1                          # 机器人尺寸，轨迹点与障碍物圆的最短距离
 
     ox = [0, -0.3, 0.9, -0.9]                   # 障碍物横坐标
     oy = [-0.3, 0.6, 0.6, -0.9]                 # 障碍物纵坐标
 
-    obstacle_radius = [0.2, 0.5, 0.3, 0.3]      # 障碍物半径
+    obstacle_radius = [0.2, 0.5, 0.3, 0.3]      #障碍物半径
 
-    if show_animation:
-        draw_axis(sx, sy, gx, gy, ox, oy, xmin, xmax, ymin, ymax, obstacle_radius)
-
+    draw_axis(sx, sy, gx, gy, ox, oy, xmin, xmax, ymin, ymax, obstacle_radius)
+    
     # 返回路径点
     rx, ry = potential_field_planning(
         sx, sy, gx, gy, ox, oy, grid_size, robot_radius, obstacle_radius, xmin, xmax, ymin, ymax)
 
-    points = np.array([[rx[0], ry[0]], [rx[1], ry[1]]])             # 创建路径的二维数组
+    
+    points = np.array([[rx[0], ry[0]], [rx[1], ry[1]]])     #创建路径的二维数组
 
     for j in range(2, len(rx), 1):
         a = np.array([[rx[j], ry[j]]])
         points = np.concatenate((points, a), axis=0)
 
     x, y = points[:, 0], points[:, 1]
-    bx, by = evaluate_bezier(points, len(rx))                       # 平滑
-
+    bx, by = evaluate_bezier(points, len(rx))                    #平滑
+    
     cyaw = []
     for i in range(len(bx)-1):
         if bx[i+1]-bx[i] ==0:
@@ -212,11 +212,10 @@ def main():
         cyaw.append(yaw)
     cyaw.append(cyaw[-1])
 
-    if show_animation:
-        plt.plot(bx, by, 'g-')
-        plt.grid()
-        plt.show()
+    plt.plot(bx, by, 'g-')
 
+    if show_animation:
+        plt.show()
 
 if __name__ == '__main__':
     print(__file__ + " start!!")
